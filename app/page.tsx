0c60e56c926647c4f31c64cda2b10611a805e1d0
Montywilliams <3650@holbertonschool.com>
2023-08-25 16:47:43 -0600

    add greeting

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,12 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          </main>
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 💩&nbsp;
+        <span className="whitespace-nowrap">
+          I'm <span className="font-bold">🎸&nbsp; Playin Monty!</span>
+        </span>
+      </p>
+    </main>
   )
 }
