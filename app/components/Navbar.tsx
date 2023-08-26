acf6eeddf94b78be6f3c0290579f6949eba9b524
Montywilliams <3650@holbertonschool.com>
2023-08-26 06:21:27 -0600

    add links and styling for each tag

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,6 +10,20 @@ export default function Navbar() {
           Monty Williams
           </Link>
         </h1>
+        <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl">
+          <Link className="text-white/90 hover:text-white" href="https://Montywilliams.tech">
+            <FaLaptop />
+          </Link>
+          <Link className="text-white/90 hover:text-white" href="https://github.com/MontyWilliams">
+            <FaGithub />
+          </Link>
+          <Link className="text-white/90 hover:text-white" href="https://twitter.com/developermonty">
+            <FaTwitter />
+          </Link>
+          <Link className="text-white/90 hover:text-white" href="https://www.instagram.com/developermonty/">
+            <FaInstagram/>
+          </Link>
+        </div>
       </div>
     </nav>
   )
