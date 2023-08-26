0f5cf75ad580ca98a0dd33f2d9765c32cbd822e7
Montywilliams <3650@holbertonschool.com>
2023-08-26 07:59:03 -0600

    typo

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -15,5 +15,13 @@ export function getSortedPostsData() {
     const fullPath = path.join(postsDirectory, fileName);
     const fileContents = fs.readFileSync(fullPath, 'utf8');
 
+    // use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+
+    const blogPost: BlogPost = {
+      id,
+      title: matterResult.data.title,
+      date: matterResult.data.date,
+    }
   })
 }
