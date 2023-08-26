fa36f37a5d63a0206acd5c1f30a2d4fc130deffc
Montywilliams <3650@holbertonschool.com>
2023-08-26 07:50:10 -0600

    get dir names using fs.readdirSync (I wonder dose it have an async version and if its necessary here); map through the dir and get the filenames

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -5,5 +5,11 @@ import matter from 'gray-matter'
 const postsDirectory = path.join(process.cwd(), 'blogposts')
 
 export function getSortedPostsData() {
-  
+  // get file names under /posts
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
+    // remove <.md> to get file id 
+    const id = fileName.replace(/\.md$/, '');
+
+  })
 }
