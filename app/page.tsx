6d7438173037dfcad06df1ee9bba872b30b93e27
Montywilliams <3650@holbertonschool.com>
2023-08-29 15:37:22 -0600

    correction

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Posts from './components/Posts';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
           I&apos;m <span className="font-bold">🎸&nbsp; Playin Monty!</span>
         </span>
       </p>
+      <Posts />
     </main>
   )
 }
