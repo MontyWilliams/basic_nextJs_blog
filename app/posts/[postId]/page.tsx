1cd315b562afe55577871cac0c75ab879ab09289
Montywilliams <3650@holbertonschool.com>
2023-09-01 09:04:18 -0600

    import Link from next/link; add styling to return div

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,5 +1,7 @@
 import { getSortedPostsData, getPostData } from '@/lib/posts'
 import { notFound } from 'next/navigation'
+import getFormattedDate from '@/lib/getFormattedDate'
+import Link from 'next/link'
 
 
 export function generateMetadata({ params }: { params: { postId: string}}) {
@@ -28,9 +30,23 @@ export default async function Post({ params }: { params: { postId: string}}) {
   }
 
   const { title, date, contentHtml } = await getPostData(postId)
+
+  const formattedDate = getFormattedDate(date)
+  
   return (
-    <>
-    </>
+    <main className="px-6 prose prose-xl prose-slate dard:prose-invert mx-auto">
+      <h1 className="text-3xl mt-4 mb-0">{title}</h1>
+      <p className="mt-0">
+        {formattedDate}
+      </p>
+      <article>
+        <section dangerouslySetInnerHTML={{ __html: contentHtml }}>
+          <p>
+            <Link href="/"> Back to home</Link>
+          </p>
+        </section>
+      </article>
+    </main>
   )
 }
  
